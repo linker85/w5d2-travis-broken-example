456ea665d36e2c8e0c416200da0e47d18785ef55
linker85 <linker85@hotmail.com>
2016-11-16 16:28:01 -0500

    new test cases added

--- Test.php
+++ Test.php
@@ -5,6 +5,17 @@ class Test extends PHPUnit_Framework_TestCase
 	public function testOnePlusOne() {
 		$this->assertEquals(1+1,2);
   	}
+	
+	public function testFailMulOne() {
+		$this->assertEquals(1*1,2);
+  	}
+	public function testFailMulZero() {
+		$this->assertEquals(1*0,2);
+  	}
+	public function testPassDivTwo() {
+		$this->assertEquals(2/2,1);
+  	}	
+	
 }
 
 ?>
